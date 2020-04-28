0a003a3431797541b37370fb4cb731e95b80025a
hitmanet <nathan200033@gmail.com>
2020-04-28 10:22:14 +0300

    feature(jest): добавление ts в тесты
    Jira issue: VEGA-40

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   verbose: true,
   setupFilesAfterEnv: ['<rootDir>/setup-tests.ts'],
+  preset: 'ts-jest',
   moduleNameMapper: {
     '^.+\\.css$': 'identity-obj-proxy',
   },
+  coveragePathIgnorePatterns: ['/node_modules/', '/coverage/', '/types/'],
   testMatch: ['**/*.test.{ts,tsx}'],
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/*.stories.tsx'],
 };
