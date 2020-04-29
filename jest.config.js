9b287fadd9cff5d2062105664ebed6e02611ff57
hitmanet <nathan200033@gmail.com>
2020-04-29 10:29:54 +0300

    fix(jest): исправление мокания стилей
    Jira issue: VEGA-40

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,11 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/setup-tests.ts'],
   preset: 'ts-jest',
   moduleNameMapper: {
-    '^.+\\.css$': 'identity-obj-proxy',
+    moduleNameMapper: {
+      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+        '<rootDir>/__mocks__/fileMock.js',
+      '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    },
   },
   coveragePathIgnorePatterns: ['/node_modules/', '/coverage/', '/types/'],
   testMatch: ['**/*.test.{ts,tsx}'],
