601bf554c6f9653124fd061e9cdcf050f349f4da
hitmanet <nathan200033@gmail.com>
2020-04-29 11:02:50 +0300

    fix(test): перенос файла теста для хука
    Jira issue: VEGA-42

--- packages/hooks/src/use-on-click-outside/index.test.tsx
+++ packages/hooks/src/use-on-click-outside/index.test.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { render, screen } from '@testing-library/react';
 
-import { TestHookComponent } from './TestHookComponent';
+import { useOnClickOutside } from '..';
+
+type TestProps = {
+  onClickOutside: (e: MouseEvent | TouchEvent) => void;
+};
+
+export const TestHookComponent: React.FC<TestProps> = ({ onClickOutside }) => {
+  const ref = useRef(null);
+
+  useOnClickOutside(ref, onClickOutside);
+
+  return (
+    <>
+      <div ref={ref} data-testid="div-test-id">
+        test-div
+      </div>
+      <button type="button" data-testid="button-test-id">
+        test-button
+      </button>
+    </>
+  );
+};
 
 const handleClickOutside = jest.fn();
 
