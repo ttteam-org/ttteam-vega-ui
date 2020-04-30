12c54ccb00b47da15c424573a8ca45e48e0dc247
hitmanet <nathan200033@gmail.com>
2020-04-30 12:58:44 +0300

    chore(rollup): правка сборки модулей
    Jira issue: VEGA-42

--- packages/hooks/src/use-on-click-outside/index.tsx
+++ packages/hooks/src/use-on-click-outside/index.tsx
@@ -1,7 +1,7 @@
 import { RefObject, useEffect } from 'react';
 import arePassiveEventsSupported from 'are-passive-events-supported';
 
-import { useLatest } from '..';
+import { useLatest } from '../use-latest';
 
 const MOUSEDOWN = 'mousedown';
 const TOUCHSTART = 'touchstart';
