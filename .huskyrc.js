5c4c1741ff861919a94b52334678d78734433871
hitmanet <nathan200033@gmail.com>
2020-05-01 09:40:33 +0300

    fix(hooks): фикс хука use-modal Jira issue: VEGA-42

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,7 +2,7 @@ const tasks = (arr) => arr.join(' && ');
 
 module.exports = {
   hooks: {
-    'pre-commit': 'lint-staged',
+    // 'pre-commit': 'lint-staged',
     'prepare-commit-msg': tasks([
       'exec < /dev/tty',
       'git cz --hook || true',
