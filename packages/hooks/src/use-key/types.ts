1334e8010bd28beb9054fe9112eb0e306dafed9d
hitmanet <nathan200033@gmail.com>
2020-05-06 14:08:33 +0300

    refactor(hooks): рефакторинг хука use-key
    Jira issue: VEGA-42

--- packages/hooks/src/use-key/types.ts
+++ packages/hooks/src/use-key/types.ts
@@ -1,13 +1,5 @@
-export const KEYDOWN = 'keydown';
-export const KEYUP = 'keyup';
-export const KEYPRESS = 'keypress';
-
-export type HandledEvents = [typeof KEYDOWN, typeof KEYUP, typeof KEYPRESS];
-
-export type HandledEventsType = HandledEvents[number];
-
-export type PossibleEvent = {
-  [Type in HandledEventsType]: HTMLElementEventMap[Type];
-}[HandledEventsType];
+export type PossibleEvent = KeyboardEvent;
 
 export type Handler = (e: PossibleEvent) => void;
+
+export type HandledEventsType = 'keydown' | 'keypress' | 'keyup';
