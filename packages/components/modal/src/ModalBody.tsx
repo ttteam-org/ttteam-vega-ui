d533f69f4e6c99ce166974c607c6cf26149e280b
hitmanet <nathan200033@gmail.com>
2020-05-06 14:19:48 +0300

    refactor(types): рефакторинг типов модалки
    Jira issue: VEGA-42

--- packages/components/modal/src/ModalBody.tsx
+++ packages/components/modal/src/ModalBody.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import { cnModal } from './helpers/cnModal';
-import { ModalComponentProps } from './types/ModalComponentProps';
 
 import './Modal.css';
 
-type ModalBodyProps = ModalComponentProps;
+type ModalBodyProps = {
+  children: React.ReactNode;
+  className?: string;
+};
 
 export const ModalBody: React.FC<ModalBodyProps> = ({ children, className, ...rest }) => (
   <div {...rest} className={cnModal('Body').mix(className)}>
