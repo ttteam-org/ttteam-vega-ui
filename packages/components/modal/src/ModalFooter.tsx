ac54fb658dfac73925b9fb81ddea0ba60a454772
hitmanet <nathan200033@gmail.com>
2020-05-06 14:19:48 +0300

    refactor(types): рефакторинг типов модалки
    Jira issue: VEGA-42

--- packages/components/modal/src/ModalFooter.tsx
+++ packages/components/modal/src/ModalFooter.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import { cnModal } from './helpers/cnModal';
-import { ModalComponentProps } from './types/ModalComponentProps';
 
 import './Modal.css';
 
-type ModalFooterProps = ModalComponentProps;
+type ModalFooterProps = {
+  children: React.ReactNode;
+  className?: string;
+};
 
 export const ModalFooter: React.FC<ModalFooterProps> = ({ children, className, ...rest }) => (
   <footer {...rest} className={cnModal('Footer').mix(className)}>
--- packages/components/modal/src/types/ModalComponentProps.ts
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from 'react';
-
-export type ModalComponentProps = {
-  children: ReactNode;
-  className?: string;
-};
