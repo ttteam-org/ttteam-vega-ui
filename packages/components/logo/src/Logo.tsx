ad36d9f0d1112d4321bc5074e139817babffd322
Roman Gurinovich <gurinovich.rv@gmail.com>
2020-05-12 18:58:33 +0300

    feature(Logo): добавить компонент
    Jira issue: VEGA-45

--- packages/components/logo/src/Logo.tsx
+++ packages/components/logo/src/Logo.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
-type LogoProps = {};
+import { cnLogo } from './cn-logo';
+import { VegaLogo } from './VegaLogo';
 
-export const Logo: React.FC<LogoProps> = () => {
-  return <div>Лого</div>;
+import './Logo.css';
+
+type LogoProps = {
+  className?: string;
+  size?: 'm';
+};
+
+export const Logo: React.FC<LogoProps> = ({ className, size = 'm' }) => {
+  const cn = cnLogo({ size }).mix(className);
+
+  return (
+    <div className={cn}>
+      <VegaLogo />
+    </div>
+  );
 };
---
+++ packages/components/logo/src/VegaLogo.tsx
@@ -0,0 +1,127 @@
+import React from 'react';
+
+export const VegaLogo: React.FC = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="107"
+      height="148"
+      fill="none"
+      viewBox="0 0 107 148"
+    >
+      <rect
+        width="99"
+        height="99"
+        x="3.5"
+        y="20.5"
+        fill="#22272B"
+        stroke="url(#paint0_linear)"
+        rx="19.5"
+      />
+      <path
+        fill="#FAFAFA"
+        d="M21.586 100V88.797h4.078c1.25 0 2.211.229 2.883.687.672.459 1.008 1.105 1.008 1.938 0 .604-.206 1.133-.617 1.586-.407.453-.928.768-1.563.945v.031c.797.1 1.432.394 1.906.883.48.49.719 1.086.719 1.79 0 1.025-.367 1.84-1.102 2.445-.734.599-1.736.898-3.007.898h-4.305zm2.523-9.344v2.656h1.11c.52 0 .93-.124 1.226-.374.302-.256.453-.605.453-1.047 0-.823-.614-1.235-1.843-1.235h-.946zm0 4.531v2.954h1.368c.583 0 1.039-.136 1.367-.407.333-.27.5-.64.5-1.109 0-.448-.164-.8-.492-1.055-.323-.255-.776-.382-1.36-.382H24.11zM38.57 100h-6.718V88.797h6.46v2.055h-3.937v2.492h3.664v2.047h-3.664v2.562h4.195V100zM47.04 90.883h-4.15V100h-2.523V88.797h6.672v2.086zM58.406 100h-2.75l-.797-2.492h-3.984l-.79 2.492h-2.733l4.078-11.203h2.992L58.406 100zm-4.125-4.43l-1.203-3.765a4.914 4.914 0 01-.187-1.008h-.063a4.269 4.269 0 01-.195.976l-1.219 3.797h2.867zM66.625 97.906v.047h4.547V100h-7.375v-1.922l3.21-3.07c.647-.62 1.1-1.138 1.36-1.555.266-.422.399-.865.399-1.328 0-1.031-.555-1.547-1.664-1.547-.964 0-1.886.383-2.766 1.149v-2.172c.974-.63 2.073-.946 3.297-.946 1.146 0 2.039.29 2.68.868.645.572.968 1.349.968 2.328 0 1.307-.784 2.658-2.351 4.054l-2.305 2.047zM74.29 100.195c-.407 0-.746-.125-1.017-.375a1.206 1.206 0 01-.406-.922c0-.375.138-.682.414-.921.281-.24.63-.36 1.047-.36.422 0 .766.123 1.031.367.271.24.407.545.407.914 0 .38-.138.693-.414.938-.271.24-.625.359-1.063.359zM81.04 100.195c-2.668 0-4-1.872-4-5.617 0-1.943.358-3.422 1.077-4.437.724-1.021 1.771-1.532 3.14-1.532 2.605 0 3.907 1.904 3.907 5.711 0 1.896-.357 3.35-1.07 4.36-.709 1.01-1.727 1.515-3.055 1.515zm.108-9.703c-1.067 0-1.601 1.341-1.601 4.024 0 2.526.523 3.789 1.57 3.789 1.021 0 1.531-1.302 1.531-3.907 0-2.604-.5-3.906-1.5-3.906z"
+      />
+      <g filter="url(#filter0_d)">
+        <path
+          fill="#FAFAFA"
+          d="M31 38l22.667 40 22.666-40H60.467l-6.8 12-6.8-12H31zm24.436 14.925l-.07 3.448 3.049-1.498-1.89 2.864 3.36.416-3.109 1.368 2.604 2.202-3.34-.562 1.024 3.288-2.513-2.315-.884 3.33-.885-3.33-2.513 2.315 1.024-3.288-3.34.562 2.604-2.202-3.11-1.368 3.361-.416-1.89-2.864 3.05 1.498-.07-3.448 1.769 2.942 1.769-2.942z"
+        />
+      </g>
+      <mask id="mask0" width="100" height="100" x="3" y="20" maskUnits="userSpaceOnUse">
+        <rect
+          width="99"
+          height="99"
+          x="3.5"
+          y="20.5"
+          fill="#22272B"
+          stroke="url(#paint1_linear)"
+          rx="19.5"
+        />
+      </mask>
+      <g mask="url(#mask0)">
+        <g filter="url(#filter1_f)" opacity="0.1">
+          <path
+            fill="#DAE5F0"
+            d="M25.53 23.037C23.368 17.773 27.24 12 32.93 12h41.14c5.69 0 9.561 5.773 7.4 11.037A8 8 0 0174.07 28H32.93a8 8 0 01-7.4-4.963z"
+          />
+        </g>
+        <g filter="url(#filter2_f)" opacity="0.5">
+          <path
+            fill="#16191C"
+            d="M81.47 114.963c2.161 5.264-1.71 11.037-7.4 11.037H32.93c-5.69 0-9.561-5.773-7.4-11.037a8 8 0 017.4-4.963h41.14a8 8 0 017.4 4.963z"
+          />
+        </g>
+      </g>
+      <defs>
+        <filter
+          id="filter0_d"
+          width="77.333"
+          height="72"
+          x="15"
+          y="22"
+          colorInterpolationFilters="sRGB"
+          filterUnits="userSpaceOnUse"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
+          <feOffset />
+          <feGaussianBlur stdDeviation="8" />
+          <feColorMatrix values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.1 0" />
+          <feBlend in2="BackgroundImageFix" result="effect1_dropShadow" />
+          <feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
+        </filter>
+        <filter
+          id="filter1_f"
+          width="105.156"
+          height="64"
+          x="0.922"
+          y="-12"
+          colorInterpolationFilters="sRGB"
+          filterUnits="userSpaceOnUse"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur result="effect1_foregroundBlur" stdDeviation="12" />
+        </filter>
+        <filter
+          id="filter2_f"
+          width="105.156"
+          height="64"
+          x="0.922"
+          y="86"
+          colorInterpolationFilters="sRGB"
+          filterUnits="userSpaceOnUse"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur result="effect1_foregroundBlur" stdDeviation="12" />
+        </filter>
+        <linearGradient
+          id="paint0_linear"
+          x1="53"
+          x2="53"
+          y1="120"
+          y2="20"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.156" stopColor="#16191C" />
+          <stop offset="0.479" stopColor="#0181CC" />
+          <stop offset="0.813" stopColor="#16191C" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear"
+          x1="53"
+          x2="53"
+          y1="120"
+          y2="20"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.156" stopColor="#16191C" />
+          <stop offset="0.479" stopColor="#0181CC" />
+          <stop offset="0.813" stopColor="#16191C" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
