9058eb29db79cf8bd2bd42180b436f2df2180f8c
Tatyana Tsvetkova <tsvetta@gmail.com>
2020-05-14 16:25:42 +0300

    chore(cz): добалены настройки для breaking change
    Jira issue: VEGA-33

--- commitizen.js
+++ commitizen.js
@@ -1,6 +1,6 @@
 module.exports = {
   types: [
-    { value: 'feature', name: 'feature:   Добавление нового функционала' },
+    { value: 'feat', name: 'feat:      Добавление нового функционала' },
     { value: 'fix', name: 'fix:       Исправление ошибок' },
     { value: 'test', name: 'test:      Добавление тестов' },
     {
@@ -18,10 +18,12 @@ module.exports = {
     scope: 'Область изменений (опционально):',
     customScope: 'Введите вашу область:',
     subject: 'Краткое описание изменений:\n',
-    body: 'Подробное описание изменений (опционально):\n',
-    footer: 'Дополнительная информация:\n',
+    body: 'Подробное описание изменений:\n',
+    breaking: 'Описание BREAKING CHANGES для повышения мажорной версии пакета:\n',
     confirmCommit: 'Сохранить получившийся коммит?',
   },
+  skipQuestions: ['footer'],
+  allowBreakingChanges: ['feat', 'fix'],
   allowCustomScopes: true,
   subjectLimit: 100,
 };
