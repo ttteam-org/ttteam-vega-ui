5143c130e1634702b79719d8ae60d97c84c4b61b
Maksim Kononov <maksim.kononov.csssr@gmail.com>
2020-05-14 13:20:49 +0300

    chore(components): добавил @emotion
    Jira issue: VEGA-38

--- packages/components/page-footer/src/PageFooter.stories.tsx
+++ packages/components/page-footer/src/PageFooter.stories.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
+/** @jsx jsx */
+
+import { jsx } from '@emotion/core';
 import { Button } from '@gpn-prototypes/vega-button';
 import { storiesOf } from '@storybook/react';
-import { block } from 'bem-cn';
 
 import { PageFooter } from './PageFooter';
 
-import './PageFooterStories.css';
+const cssWrapper = {
+  width: '960px',
+};
 
-const cnStories = block('PageFooterStories');
+const cssExtraClass1 = {
+  'display': 'flex',
+  'justify-content': 'flex-end',
+};
+
+const cssExtraClass2 = {
+  'display': 'flex',
+  'justify-content': 'space-between',
+};
 
 storiesOf('ui/PageFooter', module)
   .add('Одна кнопка', () => (
-    <div className={cnStories('wrapper')}>
-      <PageFooter className={cnStories('case-1').toString()}>
+    <div css={cssWrapper}>
+      <PageFooter css={cssExtraClass1}>
         <Button size="m" view="primary" label="Кнопка" />
       </PageFooter>
     </div>
   ))
   .add('Две кнопки', () => (
-    <div className={cnStories('wrapper')}>
-      <PageFooter className={cnStories('case-2').toString()}>
+    <div css={cssWrapper}>
+      <PageFooter css={cssExtraClass2}>
         <div>
           <Button size="m" view="primary" label="Кнопка" />
         </div>
