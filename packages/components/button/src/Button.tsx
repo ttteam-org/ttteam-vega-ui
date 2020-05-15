cadb62ac391aabf34ca667c29f4d847e139cb405
Tatyana Tsvetkova <tsvetta@gmail.com>
2020-05-15 11:27:51 +0300

    fix(button): ломаю кнопку
    BREAKING CHANGE:
    пропсы передаются иначе бвахаха

--- packages/components/button/src/Button.tsx
+++ packages/components/button/src/Button.tsx
@@ -4,5 +4,7 @@ import { Button as BaseButton } from '@gpn-design/uikit/Button';
 type ButtonProps = React.ComponentProps<typeof BaseButton>;
 
 export const Button: React.FC<ButtonProps> = (props) => {
-  return <BaseButton {...props} />;
+  const allProps = { ...props };
+
+  return <BaseButton {...allProps} />;
 };
