3688d4fbaaf3f152fc7ac9674e18d3fa202d979f
Tatyana Tsvetkova <tsvetta@gmail.com>
2020-05-18 15:42:28 +0300

    fix(rollup): styleInject inline

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,38 @@ import path from 'path';
 import resolve from '@rollup/plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
 import typescript from 'rollup-plugin-typescript2';
-import styleInject from 'style-inject';
+// import styleInject from 'style-inject';
+
+function styleInject(css, ref) {
+  console.log('ref:', ref);
+  // eslint-disable-next-line
+  if (ref === void 0) ref = {};
+  const { insertAt } = ref;
+
+  if (!css || typeof document === 'undefined') {
+    return;
+  }
+
+  const head = document.head || document.getElementsByTagName('head')[0];
+  const style = document.createElement('style');
+  style.type = 'text/css';
+
+  if (insertAt === 'top') {
+    if (head.firstChild) {
+      head.insertBefore(style, head.firstChild);
+    } else {
+      head.appendChild(style);
+    }
+  } else {
+    head.appendChild(style);
+  }
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+}
 
 const PACKAGE_ROOT = process.cwd();
 const OUTPUT_DIR = path.join(PACKAGE_ROOT, 'dist');
