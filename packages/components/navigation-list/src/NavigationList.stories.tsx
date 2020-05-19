6b88f5d0ddad344a7808a82d6dd1f6464310ed2d
Maksim Kononov <maksim.kononov.csssr@gmail.com>
2020-05-19 16:48:28 +0300

    fix(NavigationList): исправил замечания по тестированию
    Jira issue: VEGA-27

--- packages/components/navigation-list/src/NavigationList.stories.tsx
+++ packages/components/navigation-list/src/NavigationList.stories.tsx
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 
+import { useState } from 'react';
 import { jsx } from '@emotion/core';
 import { IconCheck } from '@gpn-prototypes/vega-icons';
 import { boolean, withKnobs } from '@storybook/addon-knobs';
@@ -27,50 +28,178 @@ const knobs = (): NavigationListProps => ({
 
 storiesOf('ui/NavigationList', module)
   .addDecorator(withKnobs)
-  .add('Без иконки и разделителя', () => (
-    <div css={cssWrapper}>
-      <NavigationList {...knobs()}>
-        <NavigationList.Item active>Описание проекта</NavigationList.Item>
-        <NavigationList.Item>Участники</NavigationList.Item>
-        <NavigationList.Item>Связанные документы и файлы</NavigationList.Item>
-      </NavigationList>
-    </div>
-  ))
-  .add('C иконкой', () => (
-    <div css={cssWrapper}>
-      <NavigationList {...knobs()}>
-        <NavigationList.Item css={cssWithIcon}>
-          Описание проекта
-          <IconCheck size="s" view="success" css={cssIcon} />
-        </NavigationList.Item>
-        <NavigationList.Item active>Участники</NavigationList.Item>
-        <NavigationList.Item>Связанные документы и файлы</NavigationList.Item>
-      </NavigationList>
-    </div>
-  ))
-  .add('C разделителем внутри списка', () => (
-    <div css={cssWrapper}>
-      <NavigationList {...knobs()}>
-        <NavigationList.Item active>Описание проекта</NavigationList.Item>
-        <NavigationList.Item>Участники</NavigationList.Item>
-        <NavigationList.Item>Связанные документы и файлы</NavigationList.Item>
+  .addParameters({ metadata: { author: 'CSSSR', status: 'Approved' } })
+  .add('Без иконки и разделителя', () => {
+    const [activeItem, setActiveItem] = useState('1');
+
+    return (
+      <div css={cssWrapper}>
+        <NavigationList {...knobs()}>
+          <NavigationList.Item
+            active={activeItem === '1'}
+            onClick={(): void => {
+              setActiveItem('1');
+            }}
+          >
+            Описание проекта
+          </NavigationList.Item>
+          <NavigationList.Item
+            active={activeItem === '2'}
+            onClick={(): void => {
+              setActiveItem('2');
+            }}
+          >
+            Участники
+          </NavigationList.Item>
+          <NavigationList.Item
+            active={activeItem === '3'}
+            onClick={(): void => {
+              setActiveItem('3');
+            }}
+          >
+            Связанные документы и файлы
+          </NavigationList.Item>
+        </NavigationList>
+      </div>
+    );
+  })
+  .add('C иконкой', () => {
+    const [activeItem, setActiveItem] = useState('2');
+
+    return (
+      <div css={cssWrapper}>
+        <NavigationList {...knobs()}>
+          <NavigationList.Item
+            css={cssWithIcon}
+            active={activeItem === '1'}
+            onClick={(): void => {
+              setActiveItem('1');
+            }}
+          >
+            Описание проекта
+            <IconCheck size="s" view="success" css={cssIcon} />
+          </NavigationList.Item>
+          <NavigationList.Item
+            active={activeItem === '2'}
+            onClick={(): void => {
+              setActiveItem('2');
+            }}
+          >
+            Участники
+          </NavigationList.Item>
+          <NavigationList.Item
+            active={activeItem === '3'}
+            onClick={(): void => {
+              setActiveItem('3');
+            }}
+          >
+            Связанные документы и файлы
+          </NavigationList.Item>
+        </NavigationList>
+      </div>
+    );
+  })
+  .add('C разделителем внутри списка', () => {
+    const [activeItem, setActiveItem] = useState('1');
+
+    return (
+      <div css={cssWrapper}>
+        <NavigationList {...knobs()}>
+          <NavigationList.Item
+            active={activeItem === '1'}
+            onClick={(): void => {
+              setActiveItem('1');
+            }}
+          >
+            Описание проекта
+          </NavigationList.Item>
+          <NavigationList.Item
+            active={activeItem === '2'}
+            onClick={(): void => {
+              setActiveItem('2');
+            }}
+          >
+            Участники
+          </NavigationList.Item>
+          <NavigationList.Item
+            active={activeItem === '3'}
+            onClick={(): void => {
+              setActiveItem('3');
+            }}
+          >
+            Связанные документы и файлы
+          </NavigationList.Item>
+          <NavigationList.Delimiter />
+          <NavigationList.Item
+            active={activeItem === '4'}
+            onClick={(): void => {
+              setActiveItem('4');
+            }}
+          >
+            Похожие проекты
+          </NavigationList.Item>
+          <NavigationList.Item
+            active={activeItem === '5'}
+            onClick={(): void => {
+              setActiveItem('5');
+            }}
+          >
+            Описание
+          </NavigationList.Item>
+        </NavigationList>
+      </div>
+    );
+  })
+  .add('C разделителем между списками', () => {
+    const [activeItem, setActiveItem] = useState('1');
+
+    return (
+      <div css={cssWrapper}>
+        <NavigationList {...knobs()}>
+          <NavigationList.Item
+            active={activeItem === '1'}
+            onClick={(): void => {
+              setActiveItem('1');
+            }}
+          >
+            Описание проекта
+          </NavigationList.Item>
+          <NavigationList.Item
+            active={activeItem === '2'}
+            onClick={(): void => {
+              setActiveItem('2');
+            }}
+          >
+            Участники
+          </NavigationList.Item>
+          <NavigationList.Item
+            active={activeItem === '3'}
+            onClick={(): void => {
+              setActiveItem('3');
+            }}
+          >
+            Связанные документы и файлы
+          </NavigationList.Item>
+        </NavigationList>
         <NavigationList.Delimiter />
-        <NavigationList.Item>Похожие проекты</NavigationList.Item>
-        <NavigationList.Item>Описание</NavigationList.Item>
-      </NavigationList>
-    </div>
-  ))
-  .add('C разделителем между списками', () => (
-    <div css={cssWrapper}>
-      <NavigationList {...knobs()}>
-        <NavigationList.Item active>Описание проекта</NavigationList.Item>
-        <NavigationList.Item>Участники</NavigationList.Item>
-        <NavigationList.Item>Связанные документы и файлы</NavigationList.Item>
-      </NavigationList>
-      <NavigationList.Delimiter />
-      <NavigationList {...knobs()}>
-        <NavigationList.Item>Похожие проекты</NavigationList.Item>
-        <NavigationList.Item>Описание</NavigationList.Item>
-      </NavigationList>
-    </div>
-  ));
+        <NavigationList {...knobs()}>
+          <NavigationList.Item
+            active={activeItem === '4'}
+            onClick={(): void => {
+              setActiveItem('4');
+            }}
+          >
+            Похожие проекты
+          </NavigationList.Item>
+          <NavigationList.Item
+            active={activeItem === '5'}
+            onClick={(): void => {
+              setActiveItem('5');
+            }}
+          >
+            Описание
+          </NavigationList.Item>
+        </NavigationList>
+      </div>
+    );
+  });
