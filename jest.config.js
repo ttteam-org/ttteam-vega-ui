ff792b102a274bf69b069dc34bde91b7ce4f48a8
Tatyana Tsvetkova <tsvetta@gmail.com>
2020-05-25 22:34:35 +0300

    fix(tests): поправлены алиасы для jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const tsConfig = require('./tsconfig.json');
+
 module.exports = {
   verbose: true,
   setupFilesAfterEnv: ['<rootDir>/setup-tests.tsx'],
   preset: 'ts-jest',
   moduleNameMapper: {
+    ...pathsToModuleNameMapper(tsConfig.compilerOptions.paths, { prefix: '<rootDir>/' }),
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
