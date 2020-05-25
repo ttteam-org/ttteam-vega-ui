b36aa7450f257c0d0f280e07a4069a27588f0ba9
Oleg Denisov <c1n1kk@gmail.com>
2020-05-25 15:53:55 +0300

    refactor(page-banner): изменить имена классов, тег
    Jira issue: VEGA-30

--- packages/components/page-banner/src/PageBanner.tsx
+++ packages/components/page-banner/src/PageBanner.tsx
@@ -13,19 +13,19 @@ const cnPageBanner = block('VegaPageBanner');
 
 export const PageBanner: React.FC<PageBannerProps> = ({ className, title, description }) => {
   return (
-    <div className={cnPageBanner.mix(className)}>
-      <div className={cnPageBanner('content')}>
+    <header className={cnPageBanner.mix(className)}>
+      <div className={cnPageBanner('Content')}>
         {title && (
-          <h1 className={cnPageBanner('title')} title={title}>
+          <h1 className={cnPageBanner('Title')} title={title}>
             {title}
           </h1>
         )}
         {description && (
-          <p className={cnPageBanner('description')} title={description}>
+          <p className={cnPageBanner('Description')} title={description}>
             {description}
           </p>
         )}
       </div>
-    </div>
+    </header>
   );
 };
