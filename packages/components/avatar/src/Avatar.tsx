3b3c1ce14313a4727a54a115167ca9e47dc4a93d
hitmanet <nathan200033@gmail.com>
2020-07-08 12:14:36 +0300

    fix(avatar): ухх
    ухх
    
    BREAKING CHANGE:
    уххххх

--- packages/components/avatar/src/Avatar.tsx
+++ packages/components/avatar/src/Avatar.tsx
@@ -4,5 +4,5 @@ import { Avatar as BaseAvatar } from '@gpn-design/uikit/Avatar';
 type AvatarProps = React.ComponentProps<typeof BaseAvatar>;
 
 export const Avatar: React.FC<AvatarProps> = (props) => {
-  return <BaseAvatar size="s" {...props} />;
+  return <BaseAvatar size="m" {...props} />;
 };
