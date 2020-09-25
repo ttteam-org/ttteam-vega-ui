075281fc135ffce66223aa38b0216b67554794bd
hitmanet <nathan200033@gmail.com>
2020-09-25 15:44:09 +0300

    feat:

--- packages/components/badge/src/Badge.tsx
+++ packages/components/badge/src/Badge.tsx
@@ -4,5 +4,5 @@ import { Badge as BaseBadge } from '@gpn-design/uikit/Badge';
 type BadgeProps = React.ComponentProps<typeof BaseBadge>;
 
 export const Badge: React.FC<BadgeProps> = (props) => {
-  return <BaseBadge size="l" {...props} />;
+  return <BaseBadge size="m" {...props} />;
 };
